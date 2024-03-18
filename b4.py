4bffefecf31345d11f15f21c37f77470ad20b715
ryanJohn <rjperez@rockwellautomation.com>
2024-03-18 15:54:00 -0500

    Init of Python training repo

---
+++ b4.py
@@ -0,0 +1,2 @@
+filenames = ["1.name.txt", "2.name.txt", "3.name.txt"]
+
---
+++ main.py
@@ -0,0 +1,30 @@
+
+while True:
+    user_action = input("Type 'add', 'edit', 'del', 'show' or 'exit':").lower()
+    match user_action:
+        case 'add':
+            file = open("files/todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+            todos.append(input("Enter todo:").title() + "\n")
+            file = open("files/todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+        case 'edit':
+            print("Your todos are:", todos)
+            index = int(input("Enter index to edit:"))
+            new_todo = input("Enter new todo:").title()
+            todos[index] = new_todo
+        case 'del':
+            print("Your todos are:", todos)
+            index = int(input("Enter index to delete:"))
+            todos.pop(index)
+        case 'show' | 'display':
+            print("Your todos are:")
+            file_lines = open("files/todos.txt", "r").readlines()
+            for index, todo in enumerate(file_lines):
+                print(f"{index}. {todo}")
+        case 'exit':
+            break
+        case _:
+            print("Invalid choice")
